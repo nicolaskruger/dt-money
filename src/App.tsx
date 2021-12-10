9b2eeac401d0bc35286e4886daecd67e966ca944
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-12-10 13:05:37 -0300

    feat: instalando styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import styled from "styled-components";
 import { FC } from "react";
-
-
+import React from "react";
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`;
 export const App: FC = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="App" >
+      <Title>Hello Word</Title>
     </div>
   );
 }
