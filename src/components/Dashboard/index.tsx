adcff2ec54b299390e81dbf1e12232aa41a0c349
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-12-10 15:55:47 -0300

    feat: componente transaction table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import { Container } from "./styles";
 import { Summary } from "../Summary";
+import { TransactionTable } from "../TransactionTable";
 
 export const DashBoard: FC = () => {
     return (
         <Container>
             <Summary />
+            <TransactionTable />
         </Container>
     )
 }
---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,72 @@
+import { FC } from "react";
+import { Container } from "./styles";
+
+type Transaction = {
+    name: string,
+    price: number,
+    type: string,
+    date: Date,
+}
+
+const list: Transaction[] = [
+    {
+        name: "Desenvovimento de website",
+        price: 12_000,
+        type: "Desenvolvimento",
+        date: new Date(),
+    },
+    {
+        name: "Desenvovimento de website",
+        price: -12_000,
+        type: "Desenvolvimento",
+        date: new Date(),
+    },
+    {
+        name: "Desenvovimento de website",
+        price: 12_000,
+        type: "Desenvolvimento",
+        date: new Date(),
+    }
+]
+
+const formatDate = (date: Date): string => {
+    const getDate = (date: Date) => date.getDay().toString().padStart(2, '0');
+    const getMouth = (date: Date) => date.getMonth().toString().padStart(2, '0');
+    return `${getDate(date)}/${getMouth(date)}/${date.getFullYear()}`
+}
+
+export const TransactionTable: FC = () => {
+
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {list.map(({ name, price, type, date }, index) => (
+                        <tr>
+                            <td>
+                                {name}
+                            </td>
+                            <td className={price < 0 ? "withDraw" : "deposit"}>
+                                {`R$ ${price}`}
+                            </td>
+                            <td>
+                                {type}
+                            </td>
+                            <td>
+                                {formatDate(date)}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </Container>
+    )
+}
