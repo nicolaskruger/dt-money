4b04a2f5b5027b5c982b3499411560d1ab12808d
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-12-10 13:37:31 -0300

    feat: fonts do google font

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import styled from "styled-components";
 import { FC } from "react";
 import React from "react";
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`;
+import { GlobalStyle } from "./styles/global";
 export const App: FC = () => {
   return (
     <div className="App" >
-      <Title>Hello Word</Title>
+      <h1>Hello Word</h1>
+      <GlobalStyle />
     </div>
   );
 }
