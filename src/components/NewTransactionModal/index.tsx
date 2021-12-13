17e3d162e682cea5ba5814b8caeb6f991c524b1b
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-12-13 11:52:38 -0300

    feat: estilizando modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,12 +1,26 @@
 import { FC } from "react";
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 export const NewTransactionModal: FC<ReactModal.Props> = (props) => {
     return (
-        <Modal {...props}>
-            <h2>
-                tu eh
-            </h2>
+        <Modal
+            {...props}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+
+            <Container>
+                <h2>
+                    Cadastro transação
+                </h2>
+                <input placeholder="Titulo" type="text" name="" id="" />
+                <input placeholder="Valor" type="number" name="" id="" />
+                <input placeholder="Categoria" type="text" name="" id="" />
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
         </Modal>
     )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -52,5 +52,23 @@ export const GlobalStyle = createGlobalStyle`
       opacity: 0.6;
       cursor: not-allowed;
     }
-
+    .react-modal-overlay{
+      background-color: rgb(0,0,0,0.5);
+      position: fixed;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .react-modal-content{
+      max-width: 576px;
+      width: 100%;
+      padding: 2rem;
+      background-color: var(--background);
+      border-radius: 0.25rem;
+      position: relative;
+    }
 `;
